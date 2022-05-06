da5b6b56ece3494223af15267bc9f19e8becbe01
devthalal <thalal.hassan@neoito.com>
2022-05-06 14:48:48 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+const f-test-delete = async (req, res) => {
+
+  // health check
+  if (req.params["health"] === "health") {
+    res.write(JSON.stringify({success: true, msg: "Health check success"}))
+    res.end()
+  }
+
+  // Add your code here
+  res.write(JSON.stringify({success: true, msg: `Hello f-test-delete`}))
+  res.end()
+  
+}
+
+export default f-test-delete
